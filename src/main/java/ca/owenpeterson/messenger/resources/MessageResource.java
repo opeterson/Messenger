8ca6c59537c346ca08d8bb897ef09825b96c754b
Owen Peterson <owen@SuSE-Desktop.gateway.mts.net>
2015-06-02 20:32:41 -0500

    first commit of initial project

---
+++ src/main/java/ca/owenpeterson/messenger/resources/MessageResource.java
@@ -0,0 +1,24 @@
+package ca.owenpeterson.messenger.resources;
+
+import java.util.List;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import ca.owenpeterson.messenger.model.Message;
+import ca.owenpeterson.messenger.service.MessageService;
+
+@Path("/messages")
+public class MessageResource {
+
+	MessageService messageService = new MessageService();
+	
+	@GET
+	@Produces(MediaType.APPLICATION_XML)
+	public List<Message> getMessages() {
+		return messageService.getAllMessages();
+	}
+	
+}
