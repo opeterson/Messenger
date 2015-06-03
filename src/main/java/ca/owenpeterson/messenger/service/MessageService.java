802733ee4251245a328e1550e072aa1f7d1bd986
Owen Peterson <owen@SuSE-Desktop.gateway.mts.net>
2015-06-02 21:13:14 -0500

    Completed tutorial 16. Added a mock Database class and new endpoints in MessageService

--- src/main/java/ca/owenpeterson/messenger/service/MessageService.java
+++ src/main/java/ca/owenpeterson/messenger/service/MessageService.java
@@ -2,24 +2,44 @@ package ca.owenpeterson.messenger.service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
+import ca.owenpeterson.messenger.database.DatabaseClass;
 import ca.owenpeterson.messenger.model.Message;
 
 public class MessageService {
 	
-	public List<Message> getAllMessages() {
-		Message m1 = new Message(1L, "Hello World", "Owen");
-		Message m2 = new Message(2L, "Hello Jersey", "Owen");
-		Message m3 = new Message(3L, "Hello World", "Koushik");
-		
-		List<Message> list = new ArrayList<Message>();
-		list.add(m1);
-		list.add(m2);
-		list.add(m3);
-		
-		return list;
-		
-		
+	private Map<Long, Message> messages = DatabaseClass.getMessages();
+	
+	public MessageService() {
+		messages.put(1L, new Message(1, "Hello World", "Owen"));
+		messages.put(2L, new Message(2, "Hello Jersey", "Owen"));
 	}
-
+	
+	
+	public List<Message> getAllMessages() {
+		return new ArrayList<Message>(messages.values());		
+	}
+	
+	public Message getMessage(long id) {
+		return messages.get(id);
+	}
+	
+	public Message addMessage(Message message) {
+		message.setId(messages.size() + 1);
+		messages.put(message.getId(),  message);
+		return message;
+	}
+	
+	public Message updateMessage(Message message) {
+		if (message.getId() <= 0) {
+			return null;
+		}
+		messages.put(message.getId(), message);
+		return message;
+	}
+	
+	public Message removeMessage(long id) {
+		return messages.remove(id);
+	}
 }
