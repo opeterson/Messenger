8dc4d55af704574f7108843821607601784fdb78
Owen Peterson <owen@SuSE-Desktop.gateway.mts.net>
2015-06-04 20:48:55 -0500

    completed tutorial 19. Added PUT and DELETE endpoints for messages.

--- src/main/java/ca/owenpeterson/messenger/resources/MessageResource.java
+++ src/main/java/ca/owenpeterson/messenger/resources/MessageResource.java
@@ -3,8 +3,10 @@ package ca.owenpeterson.messenger.resources;
 import java.util.List;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -14,26 +16,38 @@ import ca.owenpeterson.messenger.model.Message;
 import ca.owenpeterson.messenger.service.MessageService;
 
 @Path("/messages")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class MessageResource {
 
 	MessageService messageService = new MessageService();
 	
 	@GET
-	@Produces(MediaType.APPLICATION_JSON)
 	public List<Message> getMessages() {
 		return messageService.getAllMessages();
 	}
 	
 	@POST
-	@Produces(MediaType.APPLICATION_JSON)
-	@Consumes(MediaType.APPLICATION_JSON)
 	public Message addMessage(Message message) {
 		return messageService.addMessage(message);		
 	}
 	
+	@PUT
+	@Path("/{messageId}")
+	public Message updateMessage(@PathParam("messageId") long messageId, Message message) {
+		message.setId(messageId);
+		return messageService.updateMessage(message);
+	}
+	
+	@DELETE
+	@Path("/{messageId}")
+	public void deleteMessage(@PathParam("messageId") long messageId) {
+		messageService.removeMessage(messageId);
+	}
+	
+	
 	@GET
 	@Path("/{messageId}")
-	@Produces(MediaType.APPLICATION_JSON)
 	public Message getMessage(@PathParam("messageId") long messageId) {
 		return messageService.getMessage(messageId);		
 	}
